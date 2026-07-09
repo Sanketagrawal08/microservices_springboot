55fbfac9af9da21f462a9ebcbe1476e3da848e85
Sanketagrawal08 <sanketagrawal6969@gmail.com>
2026-07-09 15:43:07 +0530

    rate limiter and retry

--- UserService/UserServic/src/main/java/com/example/userservice/UserServic/Controller/UserController.java
+++ UserService/UserServic/src/main/java/com/example/userservice/UserServic/Controller/UserController.java
@@ -3,6 +3,8 @@ package com.example.userservice.UserServic.Controller;
 import com.example.userservice.UserServic.entities.User;
 import com.example.userservice.UserServic.servies.UserService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -26,9 +28,15 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(user1);
     }
 
+    int trycount = 1;
+
     @GetMapping("/{userId}")
-    @CircuitBreaker(name = "ratingHotelBreaker" , fallbackMethod = "ratingHotelFallback")
+     //@CircuitBreaker(name = "ratingHotelBreaker" , fallbackMethod = "ratingHotelFallback")
+  //  @Retry(name="ratingHotelService" , fallbackMethod = "ratingHotelFallback")
+    @RateLimiter(name="ratingHotelServiceRateLimit" , fallbackMethod = "ratingHotelFallback")
     public  ResponseEntity<User> getSingleUser(@PathVariable String userId){
+        log.info("tried" + trycount);
+        trycount++;
         User user  = userService.getUser(userId);
         return ResponseEntity.ok(user);
     }
